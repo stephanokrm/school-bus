a4af405abb448c4921bdcd9bd833b56bd7ed2095
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-06-23 10:19:31 -0300

    sanctum

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Cookie\CookieValuePrefix;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -14,4 +17,23 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * @param  Request  $request
+     * @return string
+     */
+    public function getTokenFromRequest($request): string
+    {
+        $token = parent::getTokenFromRequest($request);
+
+        if (!$token && $cookie = $request->cookie('XSRF-TOKEN')) {
+            try {
+                $token = CookieValuePrefix::remove($this->encrypter->decrypt($cookie, static::serialized()));
+            } catch (DecryptException $e) {
+                $token = '';
+            }
+        }
+
+        return $token;
+    }
 }
