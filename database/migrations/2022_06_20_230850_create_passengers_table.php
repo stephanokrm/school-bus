4b1be986a93f49d7d0150f9f9ca46b6d92aa5c59
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-06-26 21:14:52 -0300

    refactor

--- database/migrations/2022_06_20_230850_create_passengers_table.php
+++ database/migrations/2022_06_20_230850_create_passengers_table.php
@@ -2,6 +2,7 @@
 
 use App\Models\Address;
 use App\Models\Driver;
+use App\Models\School;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -22,6 +23,7 @@ return new class extends Migration
             $table->enum('shift', ['M', 'A', 'N']);
             $table->foreignIdFor(Address::class)->constrained();
             $table->foreignIdFor(Driver::class)->constrained();
+            $table->foreignIdFor(School::class)->constrained();
             $table->foreignIdFor(User::class, 'responsible_id')->constrained('users');
             $table->id();
             $table->string('name');
