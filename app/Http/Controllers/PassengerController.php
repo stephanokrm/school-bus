670655cf0d291743353ba240c77133d62502ca52
Stephano Ramos <stephano.ramos.p@gmail.com>
2022-06-27 14:01:12 -0300

    refactor

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -11,19 +11,18 @@ use App\Models\Address;
 use App\Models\School;
 use App\Models\Route;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Collection;
 
 class PassengerController extends Controller
 {
     /**
-     * @return Builder[]|Collection
+     * @return array|Collection
      */
     public function index(): array|Collection
     {
         $driver = Driver::query()
-        ->whereBelongsTo(auth()->user())
-        ->first();
+            ->whereBelongsTo(auth()->user())
+            ->first();
 
         return Passenger::query()
             ->whereBelongsTo($driver)
@@ -41,43 +40,44 @@ class PassengerController extends Controller
     public function store(StorePassengerRequest $request): Passenger
     {
         $driver = Driver::query()
-        ->whereBelongsTo(auth()->user())
-        ->first();
+            ->whereBelongsTo(auth()->user())
+            ->firstOrFail();
 
-        $userRequest = $request->user;
-        $addressRequest = $request->address;
-        $passengerRequest = $request->passenger;
+        $school = School::query()->findOrFail($request->input('passenger.school_id'));
 
         $responsible = User::query()->create([
-            ...$userRequest,
-            'password' => Hash::make($userRequest['cpf']),
+            ...$request->get('user'),
+            'password' => Hash::make($request->input('user.cpf')),
         ]);
 
         $address = new Address;
-        $address->fill($addressRequest);
+        $address->fill($request->get('address'));
         $address->setAttribute('external_city_id', 1);
         $address->save();
 
-        $school = School::Find($passengerRequest['school_id']);
-
         $passenger = new Passenger;
-        $passenger->fill($passengerRequest);
+        $passenger->fill($request->get('passenger'));
+        $passenger->setAttribute('goes', $request->input('passenger.goes', false));
+        $passenger->setAttribute('returns', $request->input('passenger.returns', false));
         $passenger->responsible()->associate($responsible);
         $passenger->address()->associate($address);
         $passenger->driver()->associate($driver);
         $passenger->school()->associate($school);
         $passenger->save();
 
-        $lastRoute = Route::whereHas('passenger', function($query) use ($driver){
-            $query->whereBelongsTo($driver);
-        })->orderBy('order','DESC')
-        ->first();
-        $order = isset($lastRoute->order) ? ($lastRoute->order+1) : 1;
+        $lastRoute = Route::query()
+            ->whereHas('passenger', function ($query) use ($driver) {
+                $query->whereBelongsTo($driver);
+            })
+            ->orderBy('order', 'DESC')
+            ->first();
+
+        $order = isset($lastRoute->order) ? ($lastRoute->order + 1) : 1;
 
         $route = new Route;
+        $route->setAttribute('order', $order);
         $route->passenger()->associate($passenger);
         $route->school()->associate($school);
-        $route->setAttribute('order', $order);
         $route->save();
 
         return $passenger;
@@ -89,7 +89,7 @@ class PassengerController extends Controller
      */
     public function show(Passenger $passenger): Passenger
     {
-        return $passenger->load('address','responsible','school');
+        return $passenger->load('address', 'responsible', 'school');
     }
 
     /**
@@ -101,7 +101,7 @@ class PassengerController extends Controller
         $passengerRequest = $request->passenger;
         $userRequest = $request->user;
         $addressRequest = $request->address;
-        
+
         $school = School::Find($passengerRequest['school_id']);
 
         $passenger->fill($passengerRequest);
@@ -120,17 +120,17 @@ class PassengerController extends Controller
     }
 
     /**
-     * @param Passenger  $passenger
+     * @param  Passenger  $passenger
      */
     public function destroy(Passenger $passenger)
     {
         $address = $passenger->address_id;
         $responsible = $passenger->responsible_id;
 
-        Route::where('passenger_id',$passenger->id)->delete();
+        Route::where('passenger_id', $passenger->id)->delete();
         $passenger->delete();
         Address::find($address)->delete();
         User::find($responsible)->delete();
-        
+
     }
 }
--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -8,30 +8,33 @@ use App\Models\Route;
 use App\Models\Passenger;
 use App\Models\School;
 use App\Models\Driver;
+use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 
 class RouteController extends Controller
 {
     /**
-     * @return Builder[]|Collection
+     * @param  Request  $request
+     * @return array|Collection
      */
-    public function index():  array|Collection
+    public function index(Request $request): array|Collection
     {
-        $driver = Driver::query()
-        ->whereBelongsTo(auth()->user())
-        ->first();
-
         return Route::query()
-            ->whereHas('passenger', function($query) use ($driver){
-                $query->whereBelongsTo($driver)
-                ->whereDoesntHave('absences', function ($query) {
+            ->when($request->filled('school_id'), function ($query) use ($request) {
+                $query->whereHas('school', function ($query) use ($request) {
+                    $query->where('id', $request->get('school_id'));
+                });
+            })
+            ->whereHas('passenger', function ($query) {
+                $query->whereDoesntHave('absences', function ($query) {
                     $query->whereDate('absent_at', date('Y-m-d'));
                 });
-            })->with('passenger')
+            })
+            ->with('passenger')
             ->with('school')
             ->orderBy('order')
             ->get();
-        }
+    }
 
     /**
      *
@@ -40,12 +43,12 @@ class RouteController extends Controller
     public function store(StoreRouteRequest $request)
     {
         $driver = Driver::query()
-        ->whereBelongsTo(auth()->user())
-        ->first();
+            ->whereBelongsTo(auth()->user())
+            ->first();
 
-        Route::whereHas('passenger', function($query) use ($driver){
-                $query->whereBelongsTo($driver);
-            })->delete();
+        Route::whereHas('passenger', function ($query) use ($driver) {
+            $query->whereBelongsTo($driver);
+        })->delete();
 
         foreach ($request->all() as $key => $values) {
             $school = School::Find($values['school_id']);
